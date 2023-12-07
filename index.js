f168e0d1333a12d46b4f8776940d43e88fd04555
pitchayapornpei <pitchayapornpei35@gmail.com>
2023-12-07 13:13:03 +0700

    version-1: Workshop-1-2-3

---
+++ index.js
@@ -0,0 +1,29 @@
+// function addNumbers(num1, num2) {
+//     let sum = num1 + num2;
+//     return sum;
+// }
+// //Calling the function and storing the result in a variable
+// let result = addNumbers(5, 7);
+// console.log(result);
+
+// let result2 = addNumbers(5, 10);
+// console.log(result2);
+
+// let result3 = addNumbers(5, 20);
+// let result3Mul = result3 * 5;
+// console.log(result3Mul);
+
+function subNumbers(num1, num2) {
+    let sub = num1 - num2;
+    return sub;
+}
+//Calling the function and storing the result in a variable
+let result = subNumbers(5, 2);
+console.log(result);
+
+// let result2 = subNumbers(50, 10);
+// console.log(result2);
+
+let result3 = subNumbers(5, 2);
+let result3Mul = result3 * 3;
+console.log(result3Mul);
